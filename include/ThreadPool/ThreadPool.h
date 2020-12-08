12faf6fa510681ac668f92905b296a20ce3e94a5
Mateusz Jaworski <mateusz.z.jaworski@gmail.com>
2020-12-09 00:29:07 +0100

    added stubs

--- include/ThreadPool/ThreadPool.h
+++ include/ThreadPool/ThreadPool.h
@@ -1,4 +1,32 @@
-#ifndef THREADPOOL_THREADPOOL_H
-#define THREADPOOL_THREADPOOL_H
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
 
-#endif //THREADPOOL_THREADPOOL_H
+#include <thread>
+
+namespace mz{
+
+    class ThreadPool{
+        public:
+            ThreadPool(unsigned int threads = std::thread::hardware_concurrency());
+            ~ThreadPool();
+
+            template <typename Func, typename... Args>
+            auto execute(Func func, Args... args);
+
+            ThreadPool& operator=(ThreadPool&) = delete;
+            ThreadPool(ThreadPool&) = delete;
+
+        private:
+
+    };
+
+    ThreadPool::ThreadPool(unsigned int threads = std::thread::hardware_concurrency()){}
+    ThreadPool::~ThreadPool(){}
+
+    template <typename Func, typename... Args>
+    auto ThreadPool::execute(Func func, Args... args){}
+}
+
+
+
+#endif //THREADPOOL_H
