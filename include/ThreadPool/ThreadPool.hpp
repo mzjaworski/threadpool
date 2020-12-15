bd3d60d1bbd8da385dea9c002232c8718f5a19ab
Mateusz Jaworski <mateusz.z.jaworski@gmail.com>
2020-12-15 13:37:38 +0100

    removed debug statements

--- include/ThreadPool/ThreadPool.hpp
+++ include/ThreadPool/ThreadPool.hpp
@@ -88,7 +88,7 @@ namespace mz{
     template<typename Func, typename... Args, std::enable_if_t<std::is_nothrow_invocable_v<Func&&, Args&&...>, bool>,
             std::enable_if_t<std::is_same_v<std::invoke_result_t<Func&&, Args&&...>, void>, bool>>
     void ThreadPool::execute(Func&& func, Args&&... args){
-        std::cout << "BBB" << std::endl;
+
         //Void return type so no need to use a packaged_task
         auto task = [func = std::forward<Func>(func), ...args = std::forward<Args>(args)]() { func(args...); };
 
@@ -106,7 +106,7 @@ namespace mz{
             std::enable_if_t<std::negation_v<std::conjunction<std::is_nothrow_invocable<Func&&, Args&&...>,
             std::is_same<std::invoke_result_t<Func&&, Args&&...>, void>>>, bool>>
     [[nodiscard]] auto ThreadPool::execute(Func&& func, Args&&... args) -> std::future<decltype(func(args...))> {
-        std::cout << "AAA" << std::endl;
+
         //This function has a non-void return type, therefore we create a packaged_task in order to return a future
         // object to the calling thread
         auto task = std::packaged_task<decltype(func(args...))()>(
